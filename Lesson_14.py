b0b14a156e74e4d12143dbe3de8d8944c0d9176c
Romantalex <57593644+Romantalex@users.noreply.github.com>
2020-03-25 12:38:48 +0300

    Update Lesson_14.py

--- Lesson_14.py
+++ Lesson_14.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
+#Смотрим рейтинг фотоаппаратов с их ценами, плюсами и минусами:)
+
 URL = 'https://www.expertcen.ru/article/ratings/luchshie-zerkalnye-fotoapparati.html'
 page = requests.get(URL)
 
@@ -10,22 +12,23 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 photorate = soup.find_all('td', class_ = 'ar-cit-1')
 
+#С паттернами пришлось повозиться. Особенно номер 3: вынудил считать пробелы перед переходом на след. строку. Можно было сделать проще?
 pattern1 = r'(?:\d{2,3}\s\d{3})'
 pattern2 = r'(?<=\A\n{6}).*'
 pattern3 = r'(?<=Основные плюсы:\n{1}\s{52}\n).*'
 pattern4 = r'(?<=Минусы:\n).*'
 
-model_list = [] #
-price_list = [] # создаем список для
-plus_list = [] # список с текстами
-minus_list = [] # список с текстами
+model_list = [] # создаем список для моделей
+price_list = [] # создаем список для цен
+plus_list = [] # создаем список для плюсов
+minus_list = [] # создаем список для минусов
 
-dict_info = dict.fromkeys(['Модель', 'Цена', 'Плюсы', 'Минусы'])
+dict_info = dict.fromkeys(['Модель', 'Цена', 'Плюсы', 'Минусы']) # создаем словарь для сбора информации
 
 for rev in photorate:
 
     text_str = str(rev.text)
-    text_str.strip()
+    text_str.strip()                      # Испытанный алгоритм
 
     price_info = re.findall(pattern1, text_str)
     price_str = str(price_info)
@@ -60,7 +63,7 @@ dict_info['Плюсы'] = plus_list
 dict_info['Минусы'] = minus_list
 
 #print(dict_info)
-#+ используя pandas, формируем DataFrame
+#используя pandas, формируем DataFrame
 infoDF = pd.DataFrame(dict_info)
 infoDF.to_csv('infoDF.csv') # создаем csv
 
